73fdc8748696043a9d834b01ea3db00bf08a4d88
Nick Kosarev <hmbanan666@hotmail.com>
2023-10-15 13:01:17 +0200

    health check route

---
+++ src/app/api/health/route.ts
@@ -0,0 +1,12 @@
+export async function GET() {
+  // TODO: check API
+  // const res = await fetch("/", {
+  //   headers: {
+  //     "content-type": "application/json",
+  //   },
+  // });
+  // const data = await res.json();
+  // return Response.json({ data });
+
+  return Response.json({ ok: true });
+}
