c06a971979a3a3c06658126ee54c31e790477e44
Nick Kosarev <hmbanan666@hotmail.com>
2023-11-13 12:38:14 +0200

    fix

--- src/components/CheckoutLineBlock.tsx
+++ src/components/CheckoutLineBlock.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import Link from "next/link";
 import Image from "next/image";
 import type { CheckoutLine } from "@next-orders/api-sdk";
--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+
 import { IconMinus, IconPlus } from "@tabler/icons-react";
 import {
   AddOneToCheckoutLine,
