2f400df236cf36d862d62e51a68d138f22e23103
Nick Kosarev <hmbanan666@hotmail.com>
2023-12-15 16:31:08 +0200

    Breadcrumbs visual fix

--- src/components/Breadcrumbs.tsx
+++ src/components/Breadcrumbs.tsx
@@ -22,7 +22,10 @@ export const Breadcrumbs = async ({ links }: BreadcrumbsProps) => {
   return (
     <div className="mb-6 flex flex-row justify-between items-center">
       <nav className="hidden lg:block">
-        <ol role="list" className="flex flex-row">
+        <ol
+          role="list"
+          className="flex flex-row flex-wrap gap-y-2 items-center"
+        >
           {breadcrumbItems}
         </ol>
       </nav>
@@ -37,10 +40,10 @@ type BreadcrumbItemProps = {
 };
 
 const BreadcrumbItem = ({ link }: BreadcrumbItemProps) => (
-  <li className="relative after:content-['/'] after:px-1 after:text-lg after:text-zinc-300 last:after:content-['']">
+  <li className="relative max-w-[20rem] after:content-['/'] after:px-1 after:text-lg after:text-zinc-300 last:after:content-['']">
     <Link
       href={link.href}
-      className="px-3 py-2 inline-block rounded-xl lg:hover:bg-zinc-200 lg:hover:scale-95 active:scale-90 duration-200 bg-zinc-50 data-[active=true]:bg-zinc-50"
+      className="px-3 py-2 inline-block leading-tight rounded-xl lg:hover:bg-zinc-200 lg:hover:scale-95 active:scale-90 duration-200 bg-zinc-50 data-[active=true]:bg-zinc-50"
       data-active={link.href === "#"}
     >
       {link.title}
