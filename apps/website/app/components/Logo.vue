e0ca81fe5d468e4fa8c23e53ab98fb875d27a41d
Nick Kosarev <hmbanan666@hotmail.com>
2024-09-02 16:58:59 +0200

    feat: new logo, mike and jake (#172)

--- apps/website/app/components/Logo.vue
+++ apps/website/app/components/Logo.vue
@@ -1,8 +1,13 @@
 <template>
-  <NuxtLink to="/" class="flex items-center gap-2 leading-tight hover:opacity-80 duration-200">
-    <img src="~/assets/img/logo.png" alt="Logo" class="h-12 w-12">
-    <p class="text-gray-700 dark:text-white font-bold text-xl">
-      Next Orders
-    </p>
-  </NuxtLink>
+  <ClientOnly>
+    <NuxtLink to="/" class="flex items-center gap-2 leading-tight hover:opacity-80 duration-200">
+      <img alt="Logo" src="~/assets/img/logo-full.png" class="h-12 w-auto" :class="{ hidden: isDark }">
+      <img alt="Logo Dark" src="~/assets/img/logo-full-dark.png" class="h-12 w-auto" :class="{ hidden: !isDark }">
+    </NuxtLink>
+  </ClientOnly>
 </template>
+
+<script setup lang="ts">
+const colorMode = useColorMode()
+const isDark = computed(() => colorMode.value === 'dark')
+</script>
