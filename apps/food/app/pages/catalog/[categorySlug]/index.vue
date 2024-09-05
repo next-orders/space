c2973ae038af8f086d54c894610dd6c6b0f50d6a
Nick Kosarev <hmbanan666@hotmail.com>
2024-09-05 17:51:37 +0000

    chore: category page

--- apps/food/app/pages/catalog/[categorySlug]/index.vue
+++ apps/food/app/pages/catalog/[categorySlug]/index.vue
@@ -1,8 +1,34 @@
 <template>
-  {{ slug }}
+  <Breadcrumbs :breadcrumbs="breadcrumbs" />
+
+  <h1 class="text-3xl font-medium">
+    {{ category?.name }}
+  </h1>
+  <div>Здесь представлены все товары из этой категории</div>
+
+  <div class="mt-4 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-4 gap-2">
+    <ProductCard v-for="product in category?.products" :key="product.id" :product-id="product.id" />
+  </div>
 </template>
 
 <script setup lang="ts">
-const route = useRoute()
-const slug = route.params.categorySlug
+definePageMeta({
+  validate: async ({ params }) => {
+    const { error } = await useFetch(`/api/category/slug/${params.categorySlug}`)
+    return error.value === undefined
+  },
+})
+
+const { params } = useRoute()
+const slug = params.categorySlug
+
+const { data: category } = await useFetch(`/api/category/slug/${slug}`)
+
+const breadcrumbs = [
+  { title: 'Главная', href: '/' },
+  {
+    title: category.value?.name ?? '',
+    href: '#',
+  },
+]
 </script>
