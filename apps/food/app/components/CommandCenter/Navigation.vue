d0b155ce21f7095bf44cb04459d9cf6c4ad9e8df
Nick Kosarev <hmbanan666@hotmail.com>
2024-09-09 16:58:44 +0000

    chore: shell for command center

---
+++ apps/food/app/components/CommandCenter/Navigation.vue
@@ -0,0 +1,68 @@
+<template>
+  <nav
+    :data-active="isNavbarOpened"
+    class="z-10 w-0 invisible 2xl:visible 2xl:w-72 fixed top-16 data-[active=true]:w-full data-[active=true]:visible md:data-[active=true]:w-72"
+  >
+    <div class="w-full bg-gray-50 px-4 pt-4 border-r border-gray-100">
+      <div class="h-[calc(100vh-64px)] overflow-y-auto flex flex-col justify-between">
+        <div class="mb-32">
+          <div class="flex flex-row items-center pb-2">
+            <div class="font-semibold text-xl">
+              Командный центр
+            </div>
+          </div>
+
+          <CommandCenterNavigationButton v-for="(item, index) in menu" :key="index" :label="item.label" :link="item.link" :icon="item.icon" />
+        </div>
+
+        <div class="mb-8 px-2 py-3 border rounded-2xl flex flex-row gap-2 items-center">
+          <img
+            :src="userAvatar"
+            width="40"
+            height="40"
+            alt=""
+            class="w-12 h-12 rounded-full cursor-pointer hover:scale-95 duration-200"
+            @click="() => {}"
+          >
+          <div>
+            <p>Админ</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script setup lang="ts">
+const { isNavbarOpened } = useCommandCenter()
+const { icons } = useAppConfig()
+const menu = [
+  {
+    label: 'Панель управления',
+    link: '/command-center',
+    icon: icons.dashboard,
+  },
+  {
+    label: 'Меню',
+    link: '/',
+    icon: icons.list,
+  },
+  {
+    label: 'Заявки',
+    link: '/',
+    icon: icons.checkouts,
+  },
+  {
+    label: 'Клиенты',
+    link: '/',
+    icon: icons.users,
+  },
+  {
+    label: 'Настройки',
+    link: '/',
+    icon: icons.options,
+  },
+]
+
+const userAvatar = '/burger-1.jpg'
+</script>
--- apps/food/app/layouts/commandCenter.vue
+++ apps/food/app/layouts/commandCenter.vue
@@ -1,3 +1,14 @@
 <template>
-  <slot />
+  <header class="z-20 h-16 bg-white fixed top-0 left-0 right-0">
+    <CommandCenterHeader />
+  </header>
+
+  <CommandCenterNavigation />
+
+  <main class="min-h-dvh relative w-auto bg-white top-16 2xl:pl-72">
+    <div class="min-h-[75vh] px-4 pb-10 pt-4">
+      <slot />
+    </div>
+    <Footer />
+  </main>
 </template>
