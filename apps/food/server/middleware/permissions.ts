d606283b37dcf72a0cf84faf75b543d69bd94f21
Nick Kosarev <hmbanan666@hotmail.com>
2024-09-20 13:29:12 +0000

    chore: protected routes

--- apps/food/server/middleware/permissions.ts
+++ apps/food/server/middleware/permissions.ts
@@ -2,21 +2,29 @@ export default defineEventHandler(async (event) => {
   const { user } = await getUserSession(event)
   const permissions = user?.permissions || []
 
-  // Create Product
-  if (event.path === '/api/product' && event.method === 'POST') {
-    const allowed = ['MANAGE_PRODUCTS', 'MASTER']
+  const routesWithRequiredPermissions = [
+    {
+      route: '/api/product',
+      method: 'POST',
+      permissions: ['MANAGE_PRODUCTS', 'MASTER'],
+    },
+    {
+      route: '/api/menu',
+      method: 'POST',
+      permissions: ['MANAGE_MENUS', 'MASTER'],
+    },
+    {
+      route: '/api/category',
+      method: 'POST',
+      permissions: ['MANAGE_MENUS', 'MASTER'],
+    },
+  ]
 
-    if (!allowed.some((permission) => permissions.includes(permission as PermissionCode))) {
-      throw errorResolver(createError({ statusCode: 403, statusMessage: 'Not allowed' }))
-    }
-  }
-
-  // Create Menu
-  if (event.path === '/api/menu' && event.method === 'POST') {
-    const allowed = ['MANAGE_MENUS', 'MASTER']
-
-    if (!allowed.some((permission) => permissions.includes(permission as PermissionCode))) {
-      throw errorResolver(createError({ statusCode: 403, statusMessage: 'Not allowed' }))
+  for (const route of routesWithRequiredPermissions) {
+    if (event.path === route.route && event.method === route.method) {
+      if (!route.permissions.some((permission) => permissions.includes(permission as PermissionCode))) {
+        throw errorResolver(createError({ statusCode: 403, statusMessage: 'Not allowed' }))
+      }
     }
   }
 })
