ca9797d3a9c82ebf807cb32d9a764cab81955d88
Nick Kosarev <hmbanan666@hotmail.com>
2024-10-05 16:04:35 +0000

    chore: preparations for new db

--- apps/food/app/middleware/staff.ts
+++ apps/food/app/middleware/staff.ts
@@ -1,7 +1,15 @@
 export default defineNuxtRouteMiddleware(async () => {
-  const { user } = useUserSession()
+  const { user, clear } = useUserSession()
 
-  if (!user.value?.isStaff) {
+  if (!user.value || !user.value.isStaff) {
+    return navigateTo('/command-center/sign-in')
+  }
+
+  // Check if staff has actual information
+  const { error } = await useFetch(`/api/user/${user.value.id}`)
+  if (error.value) {
+    await clear()
+
     return navigateTo('/command-center/sign-in')
   }
 })
