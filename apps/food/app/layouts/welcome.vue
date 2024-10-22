e97c4fa5a9a52e13246f2c5501b30b4b24134598
Nick Kosarev <hmbanan666@hotmail.com>
2024-10-22 17:12:29 +0000

    chore: welcome page start

---
+++ apps/food/app/layouts/welcome.vue
@@ -0,0 +1,9 @@
+<template>
+  <main class="relative w-full max-w-lg mx-auto bg-neutral-100 dark:bg-neutral-800">
+    <NuxtErrorBoundary>
+      <slot />
+    </NuxtErrorBoundary>
+
+    <Footer />
+  </main>
+</template>
---
+++ apps/food/app/pages/welcome/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="pt-24 text-center">
+    <h1 class="text-2xl">
+      Приветствуем!
+    </h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  layout: 'welcome',
+})
+</script>
